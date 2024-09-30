dba55f5d7023056ba78614078bab252f30aa0699
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-09-30 14:35:41 +0200

    use symbol BTC-USD

--- src/main.py
+++ src/main.py
@@ -1,23 +1,32 @@
+import sys
 import backtrader as bt
 import yfinance as yf
 from MyStrategy import MyStrategy
 
-#Instantiate Cerebro engine
-cerebro = bt.Cerebro()
+if __name__ == '__main__':
+    # Instantiate Cerebro engine
+    cerebro = bt.Cerebro()
 
-# Add data
-df = yf.download(
-    tickers='BTC',
-    period=None,
-    start='2022-09-28',
-    end='2024-09-28',
-    interval='1d'
-)
-data = bt.feeds.PandasData(dataname=df)
-cerebro.adddata(data) 
+    # Get data
+    symbol = 'BTC-USD'
+    df = yf.download(
+        tickers=symbol,
+        period=None,
+        start='2022-09-28',
+        end='2024-09-28',
+        interval='1d'
+    )
+    # Quit if dataframe is empty
+    if df.empty:
+        print('\nDataframe is empty for symbol : <{}>'.format(symbol))
+        print('Stop program execution')
+        sys.exit(0)
+    # Data feeds
+    data = bt.feeds.PandasData(dataname=df)
+    cerebro.adddata(data) 
 
-#Add strategy to Cerebro
-cerebro.addstrategy(MyStrategy)
+    # Add strategy to Cerebro
+    cerebro.addstrategy(MyStrategy)
 
-#Run Cerebro Engine
-cerebro.run()
+    # Run Cerebro Engine
+    cerebro.run()
